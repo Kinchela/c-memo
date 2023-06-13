c05eef61eb2bc929f0431872d712467d8670ca18
Kinchela <y1ok69nimax@gmail.com>
2023-06-14 00:26:31 +0900

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # return the suitable title per page
+  def full_title(page_title = '')
+    base_title = "C-memo"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
