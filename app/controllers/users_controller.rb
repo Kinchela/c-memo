ac75a6fe090c7ede4dd5bcbf70ff84719baed746
Kinchela <y1ok69nimax@gmail.com>
2023-07-18 20:56:31 +0900

    Finish user index and destroy action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,32 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!              #, only: [:action]
+  before_action :current_user, only: [:index, :show, :edit, :update, :destroy]
+  before_action :admin_user,     only: :destroy
 
   #helpers
   #user_signed_in?  --to verify if a user is signed in
   #current_user  --for the current signed-in user
   #user_session  --you can access the session for this scope
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+  
   def show
     @user = User.find(params[:id])
   end
   
   def new
   end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url, status: :see_other
+  end
+
+  private
+
+    def admin_user
+      redirect_to(root_url, status: :see_other) unless current_user.admin?
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_14_072423) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_18_052548) do
   create_table "children", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.date "date"
@@ -31,6 +31,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_14_072423) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.boolean "admin", default: false
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
